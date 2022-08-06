039c7ef50bb63e60bd2939f698343c48d42edbde
Kuro091 <lcminhhp@gmail.com>
2022-08-06 19:47:30 +0700

    final

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const colors = require('colors');
+const cors = require('cors');
 require('dotenv').config();
+
 const { graphqlHTTP } = require('express-graphql')
 const schema = require('./schema/schema');
 const connectDB = require('./config/db');
@@ -11,6 +13,8 @@ const app = express();
 
 //Connect to database
 connectDB();
+
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
     schema,
